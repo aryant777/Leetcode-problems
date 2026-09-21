5d61eb82bdb4900a045612742e1a1efe9a340e62
Aryan <aryan@Admins-MacBook-Air.local>
2026-09-21 23:52:19 +0530

    Initial commit: LeetCode solutions

---
+++ lc_v1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int time = 26;
+    bool day = time < 15;
+
+    if (day)
+    {printf("good morining");
+    }
+    else 
+    {printf("good evening ");}
+    return 0;
+    
+}
